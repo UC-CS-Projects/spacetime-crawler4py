be6b6c497480db1a54bebe8e93c595498562b517
Lee Chu <lwchu@uci.edu>
2023-01-30 15:45:54 -0800

    modified extract function to create hyperlink list

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
-import re
+import re, lxml
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 def scraper(url, resp):
@@ -14,8 +15,28 @@ def extract_next_links(url, resp):
     # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
+    
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    if resp.status != 200:
+        # Error case for any pages with status codes not = 200
+        print(resp.error) 
+        return list()
+
+    # Craete soupt object --> goes through page and finds html info    
+    soupt = BeautifulSoup(resp.raw_response.content, "lxml")
+
+    # Create list and set objects to store hyperlinks : nonunqiue, unique
+    hyperlink_list = list()
+    visited_set = set()
+
+    # Go through soupt list, get only hyperlinks 
+    # Example soupt value -> [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>]
+    for link in soupt.find_all("a"):
+        if link not in visited_set:
+            hyperlink_list.append(link.get("href"))
+            visited_set.add(link)
+
+    return hyperlink_list
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
